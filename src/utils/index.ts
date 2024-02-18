1f16ed4acb0c006c3987968d1198deae5dabefe9
Saad Ahmad <saadkwi12@hotmail.com>
2024-02-18 19:46:25 +1100

    update

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,31 +1,36 @@
-import {
-    APIGatewayProxyResultV2,
-} from 'aws-lambda';
+import { APIGatewayProxyStructuredResultV2 } from 'aws-lambda';
 
 export function createResponse(
-    data: Record<string, any>,
-    statusCode: number,
-): APIGatewayProxyResultV2 {
-    return {
-        statusCode,
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify(data||{}),
-    }
+  data: Record<string, any>,
+  statusCode: number,
+): APIGatewayProxyStructuredResultV2 {
+  return {
+    statusCode,
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(data || {}),
+  };
 }
 
 export function successResponse(data: Record<string, any>) {
-    return createResponse(data, 200)
+  return createResponse(data, 200);
 }
 
-export function errorResponse(error: Error, data: Record<string, any> = {}, statusCode: number = 500) {
-    return createResponse({
-        error: {
-            name: error.name,
-            message: error.message,
-            stack: error.stack,
-        },
-        ...data
-    }, statusCode)
+export function errorResponse(
+  error: Error,
+  data: Record<string, any> = {},
+  statusCode: number = 500,
+) {
+  return createResponse(
+    {
+      error: {
+        name: error.name,
+        message: error.message,
+        stack: error.stack,
+      },
+      ...data,
+    },
+    statusCode,
+  );
 }
